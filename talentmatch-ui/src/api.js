a0efc462870a6507f28d0a58921c91a44503fbdf
Kishore <your-email@gmail.com>
2026-05-05 23:50:01 +0530

    frontend deploy

--- talentmatch-ui/src/api.js
+++ talentmatch-ui/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8080"
+    baseURL: "https://talentmatch-backend-1sb6.onrender.com/api/..."
 });
 
 // ✅ Attach token automatically
