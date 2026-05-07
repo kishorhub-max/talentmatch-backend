a6d4577f2053a9c34f52a4c52fb293315c662803
Kishore <your-email@gmail.com>
2026-05-07 12:16:40 +0530

    fix cors issue

--- src/main/java/com/example/talentmatch/config/CorsConfig.java
+++ src/main/java/com/example/talentmatch/config/CorsConfig.java
@@ -2,7 +2,8 @@ package com.example.talentmatch.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.*;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -10,14 +11,20 @@ public class CorsConfig {
 
     @Bean
     public CorsFilter corsFilter() {
+
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("https://your-frontend.onrender.com");
+
+        config.addAllowedOrigin("https://talentmatch-frontend-fznr.onrender.com");
+
         config.addAllowedHeader("*");
+
         config.addAllowedMethod("*");
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
         source.registerCorsConfiguration("/**", config);
 
         return new CorsFilter(source);
